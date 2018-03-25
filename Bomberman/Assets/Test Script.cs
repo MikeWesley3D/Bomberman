324bbe58c3db8cb56ce754484cccfde912ec6c1c
MikeWesley3D <mikewesley3d@gmail.com>
2018-03-24 23:53:49 -0600

    Test script added

---
+++ Bomberman/Assets/Test Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this is a test yo
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
